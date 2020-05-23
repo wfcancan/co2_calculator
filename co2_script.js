93b7b90e546aa4d3591b12a5e0b10048de7302f9
wfcancan <39888820+wfcancan@users.noreply.github.com>
2020-05-23 14:06:38 -0300

    Update co2_script.js

--- co2_script.js
+++ co2_script.js
@@ -1,6 +1,8 @@
     var map = new OpenLayers.Map("map");
     map.addLayer(new OpenLayers.Layer.OSM());
 
+    alert('teste');
+
     var zoom=5;
     var size = new OpenLayers.Size(21,25);
     var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
